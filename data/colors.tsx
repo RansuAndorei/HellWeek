40a006d9fbeb79c946876a43de3060417414fdcf
RansuAndorei <lancejuat26@gmail.com>
2022-03-02 15:08:15 +0800

    Create toggle button to enable darkmode
    Add a new interface Color for the light and dark theme
    Add a dark mode feature which is a button that you can toggle dark mode on and off

---
+++ data/colors.tsx
@@ -0,0 +1,13 @@
+export const lightTheme = {
+  backgroundColor: "white",
+  divBackgroundColor: "light",
+  textColor: "black",
+  buttonColor: "dark",
+};
+
+export const darkTheme = {
+  backgroundColor: "black",
+  divBackgroundColor: "secondary",
+  textColor: "white",
+  buttonColor: "light",
+};
---
+++ types.tsx
@@ -0,0 +1,22 @@
+export interface Food {
+  id: number;
+  name: string;
+  description: string;
+  rating: number;
+  image: string;
+}
+
+export interface Movie {
+  id: number;
+  name: string;
+  description: string;
+  rating: number;
+  image: string;
+}
+
+export interface Color {
+  backgroundColor: string;
+  divBackgroundColor: string;
+  textColor: string;
+  buttonColor: string;
+}
